ce0f6cd599123204f3cba52e7d9dbc42730e5628
PArcos <parcos@outlook.com>
2018-01-19 17:11:40 +0100

    Testing and command parsing

--- color_correct.py
+++ color_correct.py
@@ -15,11 +15,11 @@ def stretch_pre(nimg):
     return nimg.transpose(1, 2, 0)
 
 def grey_world(nimg):
-    nimg = nimg.transpose(2, 0, 1).astype(np.uint32)
+    nimg = nimg.transpose(2, 0, 1)
     mu_g = np.average(nimg[1])
     nimg[0] = np.minimum(nimg[0]*(mu_g/np.average(nimg[0])),255)
     nimg[2] = np.minimum(nimg[2]*(mu_g/np.average(nimg[2])),255)
-    return  nimg.transpose(1, 2, 0).astype(np.uint8)
+    return  nimg.transpose(1, 2, 0)
 
 def max_white(nimg):
     if nimg.dtype==np.uint8:
--- net.py
+++ net.py
@@ -8,31 +8,32 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 from matplotlib import pyplot as plt
+from keras.utils import plot_model
 import color_correct as cc
 
 
 def adjust_image(img):
-    return cc.max_white(img)
+    return img
+    # return cc.stretch(img)
+
 
 
 # Taken from https://github.com/keras-team/keras/issues/5862
-def split_dataset(data_dir, training_data_dir, validata_data_dir, testing_data_dir, validation_split, test_split):
+def split_dataset(data_dir, training_data_dir, validation_data_dir, testing_data_dir, validation_split, test_split):
     # Recreate testing and training directories
-    if testing_data_dir.count('/') > 1:
-        shutil.rmtree(testing_data_dir, ignore_errors=False)
-        os.makedirs(testing_data_dir)
-        print("Successfully cleaned directory " + testing_data_dir)
+    shutil.rmtree(testing_data_dir, ignore_errors=False)
+    os.makedirs(testing_data_dir)
+    print("Successfully cleaned directory " + testing_data_dir)
 
-    if training_data_dir.count('/') > 1:
-        shutil.rmtree(training_data_dir, ignore_errors=False)
-        os.makedirs(training_data_dir)
-        print("Successfully cleaned directory " + training_data_dir)
-    
 
-    if validation_data_dir.count('/') > 1:
-        shutil.rmtree(validation_data_dir, ignore_errors=False)
-        os.makedirs(validation_data_dir)
-        print("Successfully cleaned directory " + validation_data_dir)
+    shutil.rmtree(training_data_dir, ignore_errors=False)
+    os.makedirs(training_data_dir)
+    print("Successfully cleaned directory " + training_data_dir)
+
+
+    shutil.rmtree(validation_data_dir, ignore_errors=False)
+    os.makedirs(validation_data_dir)
+    print("Successfully cleaned directory " + validation_data_dir)
    
 
     num_training_files = 0
@@ -66,7 +67,7 @@ def split_dataset(data_dir, training_data_dir, validata_data_dir, testing_data_d
                 shutil.copy(input_file, validation_data_dir + '/' + category_name + '/' + file)
                 num_validation_files += 1
             elif  r >= validation_split and r < validation_split + test_split:
-                shutil.copy(input_file, test_data_dir + '/' + category_name + '/' + file)
+                shutil.copy(input_file, testing_data_dir + '/' + category_name + '/' + file)
                 num_testing_files += 1
             else:
                 shutil.copy(input_file, training_data_dir + '/' + category_name + '/' + file)
@@ -79,71 +80,9 @@ def split_dataset(data_dir, training_data_dir, validata_data_dir, testing_data_d
     return num_training_files, num_validation_files, num_testing_files
 
 
-# Check https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=' Algorithm')
-    parser.add_argument("--train", action="store_true")
-    parser.add_argument("--test", action="store_true")
-    args = parser.parse_args()
-
-    batch_size = 16
-    img_width, img_height = 64, 64
-
-    train_data_dir = 'data/train'
-    validation_data_dir = 'data/validation'
-    test_data_dir = 'data/test'
-    test_split = 0.13
-    validation_split = 0.17
-
-    train_samples, validation_samples, test_samples = split_dataset('data/original', train_data_dir, validation_data_dir, test_data_dir, validation_split, test_split)
 
 
-    if K.image_data_format() == 'channels_first':
-        input_shape = (3, img_width, img_height)
-    else:
-        input_shape = (img_width, img_height, 3)
-
-
-    train_datagen = ImageDataGenerator(
-        rotation_range=40,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        rescale=1. / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        preprocessing_function=adjust_image,
-        fill_mode='nearest')
-
-    validation_datagen = ImageDataGenerator(rescale=1. / 255,
-        preprocessing_function=adjust_image)
-
-    test_datagen = ImageDataGenerator(rescale=1. / 255,
-        preprocessing_function=adjust_image)
-
-
-    train_generator = train_datagen.flow_from_directory(
-        train_data_dir,  # this is the target directory
-        target_size=(img_width, img_height),
-      #  save_to_dir='data/generated/train',
-        batch_size=batch_size,
-        class_mode='categorical')
-
-    validation_generator = validation_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-      #  save_to_dir='data/generated/validation',
-        batch_size=batch_size,
-        class_mode='categorical')
-
-    test_generator = test_datagen.flow_from_directory(
-        test_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode='categorical')
-
-    epochs = 32
-
+def create_model(input_shape):
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation(K.relu))
@@ -166,41 +105,130 @@ if __name__ == '__main__':
     model.add(Activation(K.softmax))
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
+                optimizer='rmsprop',
+                metrics=['accuracy'])
 
-    history = model.fit_generator(
-        train_generator,
-        steps_per_epoch=train_samples // batch_size,
-        epochs=epochs,
-        validation_data=validation_generator,
-        validation_steps=validation_samples // batch_size,
-        workers=8)
+    return model
 
-    # always save your weights after training
-    model.save_weights('first_try.h5')
+def preprocess(validation_split, test_split):
+    original_data_dir = 'data/original'
+    train_data_dir = 'data/train'
+    validation_data_dir = 'data/validation'
+    test_data_dir = 'data/test'
 
-    model.evaluate_generator(
+    train_samples, validation_samples, test_samples = split_dataset(original_data_dir, train_data_dir, validation_data_dir, test_data_dir, validation_split, test_split)
+
+
+    train_datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        preprocessing_function=adjust_image,
+        fill_mode='nearest')
+
+    validation_datagen = ImageDataGenerator(rescale=1. / 255,
+        preprocessing_function=adjust_image
+        )
+
+    test_datagen = ImageDataGenerator(rescale=1. / 255,
+        preprocessing_function=adjust_image
+        )
+
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,  # this is the target directory
+        target_size=(img_width, img_height),
+    #   save_to_dir='data/generated/train',
+        batch_size=batch_size,
+        class_mode='categorical')
+
+    validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+    #   save_to_dir='data/generated/validation',
+        batch_size=batch_size,
+        class_mode='categorical')
+
+    test_generator = test_datagen.flow_from_directory(
+        test_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+    return (train_samples, train_generator), (validation_samples, validation_generator), (test_samples, test_generator)
+
+# Check https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='reCIcle Algorithm')
+    parser.add_argument("weight_file")
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--test", action="store_true")
+    parser.add_argument("-epochs", default=32)
+    parser.add_argument("-batch_size", default=16)
+    parser.add_argument("-size", default=64)
+    args = parser.parse_args()
+
+    epochs = args.epochs
+    batch_size = args.batch_size
+    img_width, img_height = args.size, args.size
+    weight_file = args.weight_file
+
+    test_split = 0.13
+    validation_split = 0.17
+
+    (train_samples, train_generator), (validation_samples, validation_generator), (test_samples, test_generator) = preprocess(validation_split, test_split)
+    
+    if K.image_data_format() == 'channels_first':
+        input_shape = (3, img_width, img_height)
+    else:
+        input_shape = (img_width, img_height, 3)
+
+
+    model = create_model(input_shape)
+
+    if(not os.path.exists(weight_file)):
+        history = model.fit_generator(
+            train_generator,
+            steps_per_epoch=train_samples // batch_size,
+            epochs=epochs,
+            validation_data=validation_generator,
+            validation_steps=validation_samples // batch_size,
+            workers=8)
+        
+        # always save weights after training
+        model.save_weights(weight_file)
+
+        # Loss Curves
+        plt.figure(figsize=[8, 6])
+        plt.plot(history.history['loss'], 'r', linewidth=3.0)
+        plt.plot(history.history['val_loss'], 'b', linewidth=3.0)
+        plt.legend(['Training loss', 'Validation Loss'], fontsize=18)
+        plt.xlabel('Epochs ', fontsize=16)
+        plt.ylabel('Loss', fontsize=16)
+        plt.title('Loss Curves', fontsize=16)
+
+        # Accuracy Curves
+        plt.figure(figsize=[8, 6])
+        plt.plot(history.history['acc'], 'r', linewidth=3.0)
+        plt.plot(history.history['val_acc'], 'b', linewidth=3.0)
+        plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=18)
+        plt.xlabel('Epochs ', fontsize=16)
+        plt.ylabel('Accuracy', fontsize=16)
+        plt.title('Accuracy Curves', fontsize=16)
+
+        plt.show()
+
+    else:
+        model.load_weights(weight_file)
+
+
+    metrics = model.evaluate_generator(
         test_generator, 
         steps = test_samples // batch_size,
         workers = 8)
 
-    # Loss Curves
-    plt.figure(figsize=[8, 6])
-    plt.plot(history.history['loss'], 'r', linewidth=3.0)
-    plt.plot(history.history['val_loss'], 'b', linewidth=3.0)
-    plt.legend(['Training loss', 'Validation Loss'], fontsize=18)
-    plt.xlabel('Epochs ', fontsize=16)
-    plt.ylabel('Loss', fontsize=16)
-    plt.title('Loss Curves', fontsize=16)
-
-    # Accuracy Curves
-    plt.figure(figsize=[8, 6])
-    plt.plot(history.history['acc'], 'r', linewidth=3.0)
-    plt.plot(history.history['val_acc'], 'b', linewidth=3.0)
-    plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=18)
-    plt.xlabel('Epochs ', fontsize=16)
-    plt.ylabel('Accuracy', fontsize=16)
-    plt.title('Accuracy Curves', fontsize=16)
-
-    plt.show()
+    print(metrics)
