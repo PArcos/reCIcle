4e986e7592f3bbf85d8f026da4bcf0c0dca896b4
PArcos <parcos@outlook.com>
2018-01-22 21:38:10 +0100

    added instructions and confusion matrix

--- net.py
+++ net.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import shutil
 import os
 import numpy as np
@@ -8,15 +9,47 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
 from keras.utils import plot_model
 import color_correct as cc
-
+import cv2
 
 def adjust_image(img):
+    # return cc.max_white(img)
     return img
-    # return cc.stretch(img)
 
+# Taken from http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
 
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+    plt.show()
 
 # Taken from https://github.com/keras-team/keras/issues/5862
 def split_dataset(data_dir, training_data_dir, validation_data_dir, testing_data_dir, validation_split, test_split):
@@ -74,11 +107,10 @@ def split_dataset(data_dir, training_data_dir, validation_data_dir, testing_data
                 num_training_files += 1
 
     print("Processed " + str(num_training_files) + " training files.")
-    print("Processed " + str(num_validation_files) + " testing files.")
+    print("Processed " + str(num_validation_files) + " validation files.")
     print("Processed " + str(num_testing_files) + " testing files.")
 
     return num_training_files, num_validation_files, num_testing_files
-
 
 
 
@@ -110,6 +142,9 @@ def create_model(input_shape):
 
     return model
 
+
+
+
 def preprocess(validation_split, test_split):
     original_data_dir = 'data/original'
     train_data_dir = 'data/train'
@@ -131,11 +166,11 @@ def preprocess(validation_split, test_split):
         fill_mode='nearest')
 
     validation_datagen = ImageDataGenerator(rescale=1. / 255,
-        preprocessing_function=adjust_image
+        preprocessing_function=adjust_image,
         )
 
     test_datagen = ImageDataGenerator(rescale=1. / 255,
-        preprocessing_function=adjust_image
+        preprocessing_function=adjust_image,
         )
 
 
@@ -151,12 +186,14 @@ def preprocess(validation_split, test_split):
         target_size=(img_width, img_height),
     #   save_to_dir='data/generated/validation',
         batch_size=batch_size,
+        shuffle=False,
         class_mode='categorical')
 
     test_generator = test_datagen.flow_from_directory(
         test_data_dir,
         target_size=(img_width, img_height),
         batch_size=batch_size,
+        shuffle=False,
         class_mode='categorical')
 
     return (train_samples, train_generator), (validation_samples, validation_generator), (test_samples, test_generator)
@@ -167,9 +204,11 @@ if __name__ == '__main__':
     parser.add_argument("weight_file")
     parser.add_argument("--train", action="store_true")
     parser.add_argument("--test", action="store_true")
-    parser.add_argument("-epochs", default=32)
-    parser.add_argument("-batch_size", default=16)
-    parser.add_argument("-size", default=64)
+    parser.add_argument("--evaluate", action="store_true")
+    parser.add_argument("-epochs", default=32, type=int)
+    parser.add_argument("-batch_size", default=16, type=int)
+    parser.add_argument("-size", default=64, type=int)
+    parser.add_argument("-image", default=None)
     args = parser.parse_args()
 
     epochs = args.epochs
@@ -179,8 +218,6 @@ if __name__ == '__main__':
 
     test_split = 0.13
     validation_split = 0.17
-
-    (train_samples, train_generator), (validation_samples, validation_generator), (test_samples, test_generator) = preprocess(validation_split, test_split)
     
     if K.image_data_format() == 'channels_first':
         input_shape = (3, img_width, img_height)
@@ -190,45 +227,99 @@ if __name__ == '__main__':
 
     model = create_model(input_shape)
 
-    if(not os.path.exists(weight_file)):
-        history = model.fit_generator(
-            train_generator,
-            steps_per_epoch=train_samples // batch_size,
-            epochs=epochs,
-            validation_data=validation_generator,
-            validation_steps=validation_samples // batch_size,
-            workers=8)
-        
-        # always save weights after training
-        model.save_weights(weight_file)
 
-        # Loss Curves
-        plt.figure(figsize=[8, 6])
-        plt.plot(history.history['loss'], 'r', linewidth=3.0)
-        plt.plot(history.history['val_loss'], 'b', linewidth=3.0)
-        plt.legend(['Training loss', 'Validation Loss'], fontsize=18)
-        plt.xlabel('Epochs ', fontsize=16)
-        plt.ylabel('Loss', fontsize=16)
-        plt.title('Loss Curves', fontsize=16)
+    if args.train:
+        if weight_file is not None and weight_file is not os.path.exists(weight_file):            
+            (train_samples, train_generator), (validation_samples, validation_generator), (test_samples, test_generator) = preprocess(validation_split, test_split)
+            history = model.fit_generator(
+                train_generator,
+                steps_per_epoch=train_samples // batch_size,
+                epochs=epochs,
+                validation_data=validation_generator,
+                validation_steps=validation_samples // batch_size,
+                workers=8)
+            
+            # always save weights after training
+            model.save_weights(weight_file)
 
-        # Accuracy Curves
-        plt.figure(figsize=[8, 6])
-        plt.plot(history.history['acc'], 'r', linewidth=3.0)
-        plt.plot(history.history['val_acc'], 'b', linewidth=3.0)
-        plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=18)
-        plt.xlabel('Epochs ', fontsize=16)
-        plt.ylabel('Accuracy', fontsize=16)
-        plt.title('Accuracy Curves', fontsize=16)
+            # Loss Curves
+            plt.figure(figsize=[8, 6])
+            plt.plot(history.history['loss'], 'r', linewidth=3.0)
+            plt.plot(history.history['val_loss'], 'b', linewidth=3.0)
+            plt.legend(['Training loss', 'Validation Loss'], fontsize=18)
+            plt.xlabel('Epochs ', fontsize=16)
+            plt.ylabel('Loss', fontsize=16)
+            plt.title('Loss Curves', fontsize=16)
 
-        plt.show()
+            # Accuracy Curves
+            plt.figure(figsize=[8, 6])
+            plt.plot(history.history['acc'], 'r', linewidth=3.0)
+            plt.plot(history.history['val_acc'], 'b', linewidth=3.0)
+            plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=18)
+            plt.xlabel('Epochs ', fontsize=16)
+            plt.ylabel('Accuracy', fontsize=16)
+            plt.title('Accuracy Curves', fontsize=16)
 
-    else:
+            plt.show()
+
+            if args.test:
+                metrics = model.evaluate_generator(
+                    test_generator, 
+                    steps = test_samples // batch_size,
+                    workers = 8)
+                
+                print(metrics[1])
+
+                
+
+    elif args.test:
         model.load_weights(weight_file)
 
+        (train_samples, train_generator), (validation_samples, validation_generator), (test_samples, test_generator) = preprocess(validation_split, test_split)
 
-    metrics = model.evaluate_generator(
-        test_generator, 
-        steps = test_samples // batch_size,
-        workers = 8)
+        metrics = model.evaluate_generator(
+            test_generator, 
+            steps = test_samples // batch_size,
+            workers = 8)
 
-    print(metrics)
+        predictions = model.predict_generator(
+            test_generator, 
+            steps = test_samples // batch_size,
+            workers = 8)
+
+        y_true = test_generator.classes 
+        y_pred = np.argmax(predictions, axis=1)
+
+        print(metrics[1])
+        confusion_matrix = confusion_matrix(y_true, y_pred)
+
+        plot_confusion_matrix(confusion_matrix, test_generator.class_indices.keys(), normalize=True)
+
+    elif args.image is not None:
+        model.load_weights(weight_file)
+
+        img = cv2.resize(cv2.imread(args.image), (img_width, img_height))
+        img = np.expand_dims(img, axis=0)
+
+        prediction_datagen = ImageDataGenerator(rescale=1. / 255,
+            preprocessing_function=adjust_image
+            )
+
+        prediction_generator = prediction_datagen.flow(img)
+
+        probabilities = model.predict_generator(prediction_generator, verbose=1, steps=1)
+        prediction = probabilities[0].argmax()
+
+        if prediction == 0:
+            print("cardboard")
+        elif prediction == 1:
+            print("glass")
+        elif prediction == 2:
+            print("metal")
+        elif prediction == 3:
+            print("paper")
+        elif prediction == 4:
+            print("plastic")
+        elif prediction == 5:
+            print("trash")
+        
